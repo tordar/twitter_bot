24e7adfc7ec508b7c15b296d2fcde1e6e1b34aa3
tordar <tordar.tommervik@gmail.com>
2021-01-07 13:44:43 +0100

    Added text file

--- twitterbot.py
+++ twitterbot.py
@@ -6,34 +6,36 @@ import pyautogui
 import sys
 
 print("hello world")
-print("cmd-c to quit")
 
 
 driver = webdriver.Chrome(executable_path='/Users/tordartommervik/Desktop/chromedriver')
 driver.get('https://twitter.com/login')
 sleep(3)
 
-driver.find_element_by_name('session[username_or_email]').send_keys("username goes here")
+driver.find_element_by_name('session[username_or_email]').send_keys("****")
 # insert username
-driver.find_element_by_name('session[password]').send_keys('password goes here')
+driver.find_element_by_name('session[password]').send_keys('****')
 # insert password
 driver.find_element_by_name('session[password]').send_keys(Keys.RETURN)
 sleep(3)
 
+lines = []
+with open("text.txt") as f:
+    text = f.readlines()
+count = 0
+for lines in text:
+    if lines == "\n":
+        continue
+    count += 1
+    print(f'line {count}: {lines}')
+    pyautogui.moveTo(456, 268, 1)
+    pyautogui.click()
+    pyautogui.write(lines)
+    pyautogui.hotkey('command', 'enter')
+    # switch command out with enter for windows
+    sleep(3)
+    # increase sleep seconds if you don't want your twitter account blocked
 
-tweet = driver.find_element_by_css_selector("br[data-text='true']")
-# The element for the tweet field
+f.close()
 
-tweet_button = driver.find_element_by_css_selector("div[data-testid='tweetButtonInline']")
-# tweet_gui_position = (711, 312)
-# The actual tweet button
-
-tweet.send_keys("this tweet was sent from a python bot")
-# tweet.send_keys(Keys.RETURN)
-sleep(3)
-pyautogui.moveTo(715, 347, 2)
-sleep(3)
-
-pyautogui.click()
-
-print(pyautogui.position())
+# To force quit program, drag mouse to far end of a corner
